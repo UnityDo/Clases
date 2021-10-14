035a11149545941a01bf1a375f366d4c51d3df85
UnityDo <63395346+UnityDo@users.noreply.github.com>
2021-10-14 17:38:52 +0200

    Add files via upload

---
+++ Ejericicios.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejericicios : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    bool EvaluaPar(float numero)
+    {
+        if (numero % 2 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
