ee8fa985c0a47d79c0be4ee95ab86ec230391845
UnityDo <63395346+UnityDo@users.noreply.github.com>
2021-10-22 21:03:40 +0200

    Update Ejericicios.cs

--- Ejericicios.cs
+++ Ejericicios.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class Ejericicios : MonoBehaviour
 {
+//Para rebote
+ float x, y;
+   public float velocidadX,velocidadY;
+    float AltoPantalla, AnchoPantalla;
+    float anchoSprite,altoSprite;
+    float direccionX=1,direccionY = 1;
+
+
     bool meVe=false, enRago=false;
     // Start is called before the first frame update
     void Start()
     {
+         AltoPantalla = Camera.main.orthographicSize * 2;
+        AnchoPantalla = AltoPantalla * Screen.width / Screen.height;
         
+
+        anchoSprite = GetComponent<SpriteRenderer>().bounds.size.x;
+        altoSprite = GetComponent<SpriteRenderer>().bounds.size.y;
     }
 
     // Update is called once per frame
@@ -34,6 +47,27 @@ public class Ejericicios : MonoBehaviour
             return true;
            }else{
         return false;
+        }
+    }
+     void Rebote(){
+        float limiteDer = AnchoPantalla / 2 - anchoSprite / 2;
+        float limireIzq = -limiteDer;
+        float limiteSup = AltoPantalla / 2 - altoSprite / 2;
+        float limiteInf = -limiteSup;
+
+        x += velocidadX*direccionX;
+        y += velocidadY*direccionY;
+
+        transform.position = new Vector2(x, y);
+        // || or logico me interesa que se cumpla alguno
+        if (x >= limiteDer || x<= limireIzq)
+        {
+            //la primera vez es negativo, positivo,negativo...
+            direccionX *= -1;
+        }
+        if(y>=limiteSup || y <= limiteInf)
+        {
+            direccionY *= -1;
         }
     }
     
