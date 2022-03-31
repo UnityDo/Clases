408175395b1db45ef7db18f590fa3c78da54226d
UnityDo <63395346+UnityDo@users.noreply.github.com>
2022-03-31 20:28:43 +0200

    Add files via upload

---
+++ FPS/Agente.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Agente : MonoBehaviour
+{
+    NavMeshAgent agente;
+    public Transform posEnemigo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agente = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Dar una posicion al agente, el agente comprueba que la posicion esta en NavMesh
+        // y si puede ir a ella, se mueve
+        agente.destination = posEnemigo.position;
+    }
+}
