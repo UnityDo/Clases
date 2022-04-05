7631cfc8bd506b86785fca75c662eaab9144a2a3
UnityDo <63395346+UnityDo@users.noreply.github.com>
2022-04-05 21:01:22 +0200

    ZombieBase

---
+++ FPS/Zombie.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Zombie : MonoBehaviour
+{
+    NavMeshAgent navmeshAgent;
+    public Transform jugador;
+    Animator animator;
+    public float rangoMelee;
+    private void Awake()
+    {
+        navmeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navmeshAgent.SetDestination(jugador.position);
+        print(navmeshAgent.desiredVelocity.magnitude);
+        animator.SetFloat("Velocidad", navmeshAgent.desiredVelocity.magnitude);
+        print(Vector3.Distance(transform.position, jugador.position));
+        if (Vector3.Distance(transform.position, jugador.position) <= rangoMelee)
+        {
+            //se para el agente
+            navmeshAgent.isStopped = true;
+            animator.SetBool("Atancado", true);
+        }
+    }
+}
