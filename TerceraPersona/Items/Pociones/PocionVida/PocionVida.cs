aa7761d1d3e8626e30d490c199b1f45cdc1f5aaa
UnityDo <63395346+UnityDo@users.noreply.github.com>
2022-05-18 18:58:43 +0200

    Clases de Items

---
+++ TerceraPersona/Items/Pociones/PocionVida/PocionVida.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Item", menuName = "Items/Pociones/PocionVida", order = 1)]
+public class PocionVida : Pociones
+{
+    //Cuanta vida recupera con la pocion de vida
+    public float vidaRecupera;
+}
