72b0635bacc958e7659dfdea87e9fa49ddc3caa8
UnityDo <63395346+UnityDo@users.noreply.github.com>
2022-05-20 18:28:50 +0200

    Delete Arma.cs

--- TerceraPersona/Arma.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Arma : Item
-{
-    public enum Manos { UnaMano, DosManos }
-    public Manos manos;
-}
