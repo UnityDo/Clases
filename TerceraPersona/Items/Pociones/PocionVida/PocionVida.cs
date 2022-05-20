84ca4f8cbf7d4acfc8eab08b2f37c69170f6cc39
UnityDo <63395346+UnityDo@users.noreply.github.com>
2022-05-20 18:29:38 +0200

    Delete TerceraPersona/Items directory

--- TerceraPersona/Items/Pociones/PocionVida/PocionVida.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName = "Item", menuName = "Items/Pociones/PocionVida", order = 1)]
-public class PocionVida : Pociones
-{
-    //Cuanta vida recupera con la pocion de vida
-    public float vidaRecupera;
-}
--- TerceraPersona/Items/Pociones/Pociones.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName = "Item", menuName = "Items/Pociones", order = 1)]
-public class Pociones : Item
-{
-    public AudioClip audioUsa;
-}
